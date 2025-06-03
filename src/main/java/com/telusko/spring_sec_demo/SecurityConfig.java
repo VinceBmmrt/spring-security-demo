9411b903d1236698c4a199288eb091e9744abbee
VinceBmmrt <vincebommert75@gmail.com>
2025-06-03 18:41:52 +0200

    Setting username and paswrd

--- src/main/java/com/telusko/spring_sec_demo/SecurityConfig.java
+++ src/main/java/com/telusko/spring_sec_demo/SecurityConfig.java
@@ -3,22 +3,35 @@ package com.telusko.spring_sec_demo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().authenticated()  // toute requête nécessite auth
-                )
-                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll                   // autorise accès à la page login par défaut
-                )
-                .csrf(AbstractHttpConfigurer::disable);     // désactive CSRF (optionnel pour API REST)
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .formLogin(withDefaults());
+
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("telusko")
+                .password("1234")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
