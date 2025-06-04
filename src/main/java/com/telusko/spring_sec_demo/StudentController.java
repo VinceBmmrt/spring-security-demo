81e3f81bbbf5ef412136ede6eae5bd8cac87cd94
VinceBmmrt <vincebommert75@gmail.com>
2025-06-04 17:20:53 +0200

    Error without CSRF Token on Post request

--- src/main/java/com/telusko/spring_sec_demo/StudentController.java
+++ src/main/java/com/telusko/spring_sec_demo/StudentController.java
@@ -1,4 +1,29 @@
 package com.telusko.spring_sec_demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class StudentController {
+
+    List<Student> students = new ArrayList<>(List.of(
+            new Student(1, "Jimmy", "java"),
+            new Student(2, "Usopp", "python")
+    ));
+
+    @GetMapping("students")
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    @PostMapping("student")
+    public void addStudent(@RequestBody Student student) {
+        students.add(student);
+    }
+
 }
