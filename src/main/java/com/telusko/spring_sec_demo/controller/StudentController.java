d9d764e39bc84dce68af5a94f1f3fd0458eb3976
VinceBmmrt <vincebommert75@gmail.com>
2025-06-10 17:43:52 +0200

    Creating a userDetails Service + UserRepository

--- src/main/java/com/telusko/spring_sec_demo/controller/StudentController.java
+++ src/main/java/com/telusko/spring_sec_demo/controller/StudentController.java
@@ -1,5 +1,6 @@
-package com.telusko.spring_sec_demo;
+package com.telusko.spring_sec_demo.controller;
 
+import com.telusko.spring_sec_demo.model.Student;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +13,7 @@ import java.util.List;
 
 @RestController
 public class StudentController {
- 
+
     List<Student> students = new ArrayList<>(List.of(
             new Student(1, "Jimmy", "java"),
             new Student(2, "Usopp", "python")
