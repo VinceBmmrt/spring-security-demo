0dc77aaa79a9de2a892152cf9b41ff4615f889d7
VinceBmmrt <vincebommert75@gmail.com>
2025-06-12 15:22:11 +0200

    Config Update from the course

--- src/main/java/com/telusko/spring_sec_demo/config/SecurityConfig.java
+++ src/main/java/com/telusko/spring_sec_demo/config/SecurityConfig.java
@@ -5,11 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,32 +20,42 @@ public class SecurityConfig {
     @Autowired
     private UserDetailsService userDetailsService;
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        // ATTENTION: Seulement pour les tests/cours !
-        // En production, utilise BCryptPasswordEncoder !
-        return NoOpPasswordEncoder.getInstance();
-    }
 
     @Bean
     public AuthenticationProvider authProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setUserDetailsService(userDetailsService);
-        provider.setPasswordEncoder(passwordEncoder());
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
         return provider;
     }
 
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(basic -> {
-                })
-                .formLogin(form -> form.permitAll())
-                .logout(logout -> logout.permitAll());
+
+        http.csrf(customizer -> customizer.disable())
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
+
+
+
+
+
+    /*
+     * @Bean public UserDetailsService userDetailsService() {
+     *
+     * UserDetails user=User .withDefaultPasswordEncoder() .username("navin")
+     * .password("n@123") .roles("USER") .build();
+     *
+     * UserDetails admin=User .withDefaultPasswordEncoder() .username("admin")
+     * .password("admin@789") .roles("ADMIN") .build();
+     *
+     * return new InMemoryUserDetailsManager(user,admin); }
+     */
+
+
 }
